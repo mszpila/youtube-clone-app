31bfbde2f21df15ef4aca29421b5cbd178e89d2f
mszpila <szpila.mikolaj@gmail.com>
2020-10-10 11:36:02 +0200

    Add unique property to the user model

--- libs/user/user_model.js
+++ libs/user/user_model.js
@@ -21,10 +21,12 @@ const UserSchema = mongoose.Schema({
 	username: {
 		type: String,
 		required: true,
+		unique: true,
 	},
 	email: {
 		type: String,
 		required: true,
+		unique: true,
 	},
 	password: {
 		type: String,
