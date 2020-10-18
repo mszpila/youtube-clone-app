ddf8551c03ec3268086ae9f6b73d648dca8d8c1e
mszpila <szpila.mikolaj@gmail.com>
2020-10-18 13:33:52 +0200

    Add whitelisted urls for CORS policy

--- index.js
+++ index.js
@@ -22,7 +22,18 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors()); // { origin: "http://localhost:3000/", credentials: true })
+
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (process.env.WHITELIST_URLS.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS policy"));
+		}
+	},
+};
+app.use(cors(corsOptions));
+
 app.use("/public", express.static("public"));
 app.use(express.static(path.join(__dirname, "client/build")));
 
